e18c7f87e0bdada2974ce5a05ca5700dc6224338
KoliosMarios <koliosmarios@gmail.com>
2022-05-15 20:32:39 +0300

    add functionality to Sign Up page

--- main.js
+++ main.js
@@ -1,98 +1,58 @@
-let presentation = document.getElementById("presentation");
-const submitBtn = document.getElementById("submit");
-
-function getTodaysEvents() {
-  fetch("https://history.muffinlabs.com/date")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      presentation.innerHTML = "";
-      const events = data.data.Events;
-      for (let i = 0; i < events.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `In year ${events[i].year}: ${events[i].text}`;
-        presentation.appendChild(li);
-      }
-    })
-    .catch((error) => {
-      alert("ERROR!");
-    });
+function movetoSignup() {
+  window.location.href = "http://127.0.0.1:5500/signUp.html";
 }
 
-function getTodaysBirths() {
-  fetch("https://history.muffinlabs.com/date")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      presentation.innerHTML = "";
-      const births = data.data.Births;
-      for (let i = 0; i < births.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `In year ${births[i].year}: ${births[i].text}`;
-        presentation.appendChild(li);
-      }
-    })
-    .catch((error) => {
-      alert("ERROR!");
-    });
+function movetoLogin() {
+  window.location.href = "http://127.0.0.1:5500/logIn.html";
 }
 
-function getTodaysDeaths() {
-  fetch("https://history.muffinlabs.com/date")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      presentation.innerHTML = "";
-      const deaths = data.data.Deaths;
-      for (let i = 0; i < deaths.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = `In year ${deaths[i].year}: ${deaths[i].text}`;
-        presentation.appendChild(li);
-      }
-    })
-    .catch((error) => {
-      alert("ERROR!");
-    });
+var validateEmail = function (address) {
+  let result = false;
+  const pattern = /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (pattern.test(address) === true) {
+    result = true;
+  }
+  return result;
+};
+
+if (localStorage.getItem("users")) {
+  var users = JSON.parse(localStorage.getItem("users"));
+} else {
+  localStorage.setItem("users", JSON.stringify([]));
+  var users = JSON.parse(localStorage.getItem("users"));
 }
 
-submitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  const day = document.getElementById("day").value;
-  const month = document.getElementById("month").value;
-  const topic = document.getElementById("topic").value;
-  fetch(`https://history.muffinlabs.com/date/${month}/${day}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      presentation.innerHTML = "";
-      if (topic === "Events") {
-        const topic = data.data.Events;
-        for (let i = 0; i < topic.length; i++) {
-          let li = document.createElement("li");
-          li.textContent = `In year ${topic[i].year}: ${topic[i].text}`;
-          presentation.appendChild(li);
-        }
-      } else if (topic === "Births") {
-        const topic = data.data.Births;
-        for (let i = 0; i < topic.length; i++) {
-          let li = document.createElement("li");
-          li.textContent = `In year ${topic[i].year}: ${topic[i].text}`;
-          presentation.appendChild(li);
-        }
-      } else if (topic === "Deaths") {
-        const topic = data.data.Deaths;
-        for (let i = 0; i < topic.length; i++) {
-          let li = document.createElement("li");
-          li.textContent = `In year ${topic[i].year}: ${topic[i].text}`;
-          presentation.appendChild(li);
-        }
-      }
-    })
-    .catch((error) => {
-      alert("ERROR!");
-    });
-});
+function signup() {
+  let e = document.getElementById("signup_email").value;
+  let p = document.getElementById("signup_password").value;
+  if (!validateEmail(e)) {
+    document.getElementById("signup_email").value = "";
+    document.getElementById("signup_password").value = "";
+    alert("Add a valid Email");
+    return;
+  }
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === e) {
+      document.getElementById("signup_email").value = "";
+      document.getElementById("signup_password").value = "";
+      alert("User already exists");
+      return;
+    }
+  }
+  if (p.length < 8) {
+    document.getElementById("signup_email").value = "";
+    document.getElementById("signup_password").value = "";
+    alert("Password needs to be longer than 7 characters");
+    return;
+  }
+  let user = {
+    email: e,
+    password: p,
+  };
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
+  document.getElementById("signup_email").value = "";
+  document.getElementById("signup_password").value = "";
+  window.location.href = "http://127.0.0.1:5500/logIn.html";
+}
